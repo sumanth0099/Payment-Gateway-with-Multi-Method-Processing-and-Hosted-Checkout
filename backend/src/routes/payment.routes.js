58e67f7b4edb7084c45e4ca0150bdf185e18a1cf
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-01-07 06:33:10 +0530

    Added get payment with id feature

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const merchantAuth = require("../middlewares/merchantAuth");
-const { createPayment } = require("../controllers/payment.controller");
+const {
+  createPayment,
+  getPaymentById
+} = require("../controllers/payment.controller");
 
 router.post("/v1/payments", merchantAuth, createPayment);
+router.get("/v1/payments/:paymentId", merchantAuth, getPaymentById);
 
 module.exports = router;
