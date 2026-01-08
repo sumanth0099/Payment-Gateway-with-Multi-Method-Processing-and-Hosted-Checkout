432628900d657763ff4a524084cf227680d5aa09
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-01-08 15:25:15 +0530

    Added getDashboard function in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,3 +5,10 @@ export async function getTestMerchant() {
   if (!res.ok) throw new Error("Merchant not found");
   return res.json();
 }
+
+export async function getDashboardStats() {
+    const res = await fetch("http://localhost:8000/api/v1/dashboard/stats");
+    if (!res.ok) throw new Error("Failed to fetch stats");
+    return res.json();
+  }
+  
