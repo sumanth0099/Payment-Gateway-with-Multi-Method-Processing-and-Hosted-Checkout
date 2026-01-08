15fe7fceb21b69f9cf07f540a5ffd620e229cdf8
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-01-08 15:36:13 +0530

    Modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
+import Transactions from "./pages/Transactions";
 function App() {
   return (
     <BrowserRouter>
